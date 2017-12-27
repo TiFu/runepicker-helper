98c4b10bc02b4fd15bd5c66e15d79b5dbfff25da
Tino Fuhrmann <tino-fuhrmann@web.de>
2017-12-27 23:01:19 +0800

    Added typed implementation of our sockets

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
 
+// TODO: update this to render the main component
 ReactDOM.render(
   <div>Node version: {process.versions.node}</div>,
   document.getElementById('content'))
