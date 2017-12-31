f893600d4da64975fac26551b68f9ecb3e21c8fd
Tino Fuhrmann <tino-fuhrmann@web.de>
2017-12-31 15:42:04 +0800

    Added rune name to tooltip

--- frontend/src/app/rune-picker/perk/perk.component.ts
+++ frontend/src/app/rune-picker/perk/perk.component.ts
@@ -19,6 +19,17 @@ export class PerkComponent implements OnInit {
     this.keystone = this.staticData.isKeystone(this.perk);
   }
 
+  getTooltip() {
+    let tooltip = "<span class=\"big\"><b>" + this.getName() + "</b></span></br>&nbsp;</br>";
+    tooltip += this.getDescription();
+    return tooltip;
+  }
+  getName() {
+    if (this.perk) {
+      return this.staticData.perksDescription[this.perk].name;
+    }
+  }
+  
   getDescription(){
     if(this.perk){
       return this.staticData.perksDescription[this.perk].longDesc;
